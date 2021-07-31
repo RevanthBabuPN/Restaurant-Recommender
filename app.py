b37d79498001460926a213a7670a161ebe4ede1c
revanthbabu <putturevanth@gmail.com>
2021-07-31 19:08:41 +0530

    added some apis

--- app.py
+++ app.py
@@ -11,21 +11,37 @@ api = Api(app)
 parser = reqparse.RequestParser()
 parser.add_argument('cuisine_list', type=str, location='json')
 
+
+class CuisineList(Resource):
+    def get(self):
+        cuisines = recommend.get_cuisines()
+        return Response(json.dumps(cuisines), status=200, mimetype="application/json")
+
+
+class RestaurantList(Resource):
+    def get(self):
+        restaurants = recommend.get_restaurants()
+        return Response(json.dumps(restaurants), status=200, mimetype="application/json")
+
+
 class Recommendation1(Resource):
-	def get(self,restaurant):
-		result = recommend.recommend_similar(restaurant)
-		return Response(result, status=200, mimetype="application/json")
+    def get(self, restaurant):
+        result = recommend.recommend_similar(restaurant)
+        return Response(result, status=200, mimetype="application/json")
+
 
 class Recommendation2(Resource):
-	def get(self):
-		cuisine_list = request.get_json()
-		# cuisine_list = parser.parse_args()
-		print(cuisine_list)
-		result = recommend.recommend_cuisine('')
-		return Response(result, status=200, mimetype="application/json")
+    def get(self):
+        user_cuisines = request.get_json()
+        # user_cuisines = parser.parse_args()
+        result = recommend.recommend_cuisine(user_cuisines)
+        return Response(result, status=200, mimetype="application/json")
 
-api.add_resource(Recommendation1, "/resrecommender/1/<string:restaurant>")
-api.add_resource(Recommendation2, "/resrecommender/2")
+
+api.add_resource(Recommendation1, "/api/resrecommender/1/<string:restaurant>")
+api.add_resource(Recommendation2, "/api/resrecommender/2")
+api.add_resource(CuisineList, "/api/resrecommender/cuisines")
+api.add_resource(RestaurantList, "/api/resrecommender/restaurants")
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
