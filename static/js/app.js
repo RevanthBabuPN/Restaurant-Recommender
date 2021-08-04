5ed6d6d246ec01df437a1d4046f077014a2974a6
revanthbabu <putturevanth@gmail.com>
2021-08-04 12:07:22 +0530

    remove localhost in url of fetch

--- static/js/app.js
+++ static/js/app.js
@@ -2,7 +2,7 @@ const form1 = document.querySelector('#form-1');
 const form2 = document.querySelector('#form-2');
 
 $(document).ready(function () {
-	fetch('http://127.0.0.1:5000/api/resrecommender/restaurants')
+	fetch('/api/resrecommender/restaurants')
 		.then(response => response.json())
 		.then(data => {
 			var content = data;
@@ -32,7 +32,7 @@ $(document).ready(function () {
 		});
 
 	
-	fetch('http://127.0.0.1:5000/api/resrecommender/cuisines')
+	fetch('/api/resrecommender/cuisines')
 		.then(response => response.json()
 		.then(data => {
 			$('.ui.dropdown')
@@ -65,7 +65,7 @@ form2.addEventListener("submit", e => {
 	if (cuisines.length > 0) {
 		form2.submit();
 	}
-	fetch('http://127.0.0.1:5000/api/resrecommender/2', options)
+	fetch('/api/resrecommender/2', options)
 		.then(response => response.json()
 		.then(data => {
 			console.log(data)
